6ebc2addeb6e3e958253c26c6ef21755ccf93c46
e.georgieva24 <e.georgieva24@LAB108PC13>
2023-09-19 15:26:31 +0300

    Add project files.

---
+++ Method Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter another number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = SumOfTwoNumbers(num1, num2);
+
+        Console.WriteLine($"The sum of two numbers is: {sum}");
+    }
+    static int SumOfTwoNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+   
+}
